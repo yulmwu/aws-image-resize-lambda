047c0c8b923c0cc247e20fc6359fe059041c3123
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-08-23 20:12:54 +0900

    updated

--- s3-deploy.sh
+++ s3-deploy.sh
@@ -2,10 +2,7 @@ cd dist
 zip -r ../dist.zip . ../node_modules
 cd ..
 
-aws s3 cp dist.zip s3://2test0820deploy/
-
 aws lambda update-function-code \
     --function-name resize-image \
     --region us-east-1 \
-    --s3-bucket 2test0820deploy \
-    --s3-key dist.zip
+    --zip-file fileb://dist.zip
